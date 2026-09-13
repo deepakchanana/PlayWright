5efa94db4223cc596037139d1c9eb32c9355d366
deepakchanana <deepakdeep1992@gmail.com>
2026-09-13 11:19:26 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+  headless:false,
+  slowMo:1000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
