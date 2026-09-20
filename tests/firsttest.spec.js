71795b5bc139655d41bcf54b0d2f76961369c07f
deepakchanana <deepakdeep1992@gmail.com>
2026-09-20 19:48:50 +0530

    Delete tests/firsttest.spec.js

--- tests/firsttest.spec.js
+++
@@ -1,5 +0,0 @@
-const { test, expect } = require('@playwright/test');
-test('Open Google website', async ({ page }) => {
-    await page.goto('https://www.google.com');
-    await expect(page).toHaveTitle(/Google/);
-});
