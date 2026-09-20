c52e18b2ef2fdf3a38bd66b3475ea1377c4e296d
deepakchanana <deepakdeep1992@gmail.com>
2026-09-20 21:35:47 +0530

    Update second.spec.js

--- tests/second.spec.js
+++ tests/second.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 test('Open Facebook', async ({ page }) => {
   await page.goto('https://www.facebook.com');
 
-  await expect(page).toHaveTitle(/facebook/);
+  await expect(page).toHaveTitle(/Facebook/);
 
   // Keep it visible for demonstration
   await page.waitForTimeout(5000);
