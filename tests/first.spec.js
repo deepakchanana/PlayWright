7bfa43dbe22cf3226c746c4f55d4c08cee422eac
deepakchanana <deepakdeep1992@gmail.com>
2026-09-20 21:34:37 +0530

    Update first.spec.js

--- tests/first.spec.js
+++ tests/first.spec.js
@@ -3,5 +3,5 @@ const { test, expect } = require('@playwright/test');
 test('Google Test', async ({ page }) => {
     await page.goto('https://www.google.com');
 
-    await expect(page).toHaveTitle(/Google/);
+    await expect(page).toHaveTitle(/google/);
 });
